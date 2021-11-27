77dce43e6aec205a391250e67b858bae9a2e0e53
noj99 <nikhilrulz5@gmail.com>
2021-11-27 23:46:18 +0530

    new function added

--- data_processing.py
+++ data_processing.py
@@ -2,10 +2,6 @@ print("adding all the data processing code related to fraud detection")
 
 
 
-a=5
-b-7
-sum(a,b):
-    c=a+b
-    return a
 
-print("Added a new function sum")
+sum:
+    return a+b
